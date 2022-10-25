d6066e627dc1e28bb78357080c4e28c181828068
Piotr Borysewicz <piotrborysewicz@yahoo.co.uk>
2022-10-25 17:01:56 +0100

    chnages

--- Session10/TASK1/script.js
+++ Session10/TASK1/script.js
@@ -8,14 +8,3 @@ function turnOff() {
     document.getElementById("bulb").src = "img/pic_bulboff.gif";
 }
 
-function hitMe() {
-    window.alert("ALERT!!!");
-}
-
-function cons() {
-    console.log(5 + 6);
-}
-
-function print() {
-    window.print!
-}
