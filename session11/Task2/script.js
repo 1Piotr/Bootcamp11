995ccf772e88f13f4ea97194ad8ec85c34ad6f01
Piotr Borysewicz <piotrborysewicz@yahoo.co.uk>
2022-10-26 12:21:08 +0100

    new

--- session11/Task2/script.js
+++ session11/Task2/script.js
@@ -1,15 +1,12 @@
 
 //Creating empty shopping list//
 let myList = [];
+
 let list =document.getElementById("myList");
 
-// function addToList1() {
-  //  let item = document.getElementById("item").value;
- //   myList.push(item);
- //
+
 function addToList() {
     let item = document.getElementById("item").value;
-    
     if (myList.includes(item)) {
 
         window.alert(item + "  already on the list");
@@ -19,11 +16,36 @@ function addToList() {
         let newElement = document.createElement("li");
         newElement.innerHTML = item;
         list.appendChild(newElement);
-        window.alert(item + "  added to the list");
+        window.alert(item + " to be added to the list");
         
     }
     document.getElementById('item').value="";
  }
+function removeFromList() {
+    let item = document.getElementById("item").value;
+    if (myList.includes(item)) {
+        // remove item from the array
+        let index = myList.indexOf(item);
+        myList.splice(index, 1)
+
+        //remove item from the html list
+        let listHtml = list.children
+        for (let i = 0; i < listHtml.length; i++) {
+            if (listHtml[i].innerHTML == item ) {
+                window.alert(item + " to be removed to the list");
+                list.removeChild(list.children[i]);        
+        }
+    }
+        
+    } else {
+        window.alert(item + "  not on the list");
+        
+        
+        
+    }
+    document.getElementById('item').value="";
+ }
+ 
 
 
 
